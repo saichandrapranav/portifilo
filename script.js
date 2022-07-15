5dfa30a3b1facf114600c8c8608c5c4412781c19
saichandrapranav <saichandrapranav@gmail.com>
2022-07-15 18:26:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.fromTo(".spider", {
+    delay: 1,
+    duration: 3,
+    x: 10
+    // scale: 1,
+    // ease: 'elastic'
+}, {
+    delay: 2,
+    duration: 5,
+    x: 620,
+    // scale: 1,
+    // ease: 'elastic',
+    rotation: 30
+
+})
+
+// gsap.fromTo(".spider", {
+//     delay: 6,
+//     duration: 5,
+//     x: 620
+// }, {
+//     // delay: 6,
+//     duration: 5,
+//     x: 10
+
+// })
+gsap.to(".spider", {
+    delay: 7,
+    autoAlpha:0
+})
+
+
